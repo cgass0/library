3894e2fc142418c42430743dd2e3bc062467ebb9
TheDawg101 <curtisgass@hotmail.com>
2022-10-26 17:48:30 -0400

    JS script added, Constructors, along with Functions

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ toggleSwitch.addEventListener('change', switchTheme, false);
 
 // New Book Menu Overlay //
 
+const addBookBtn = document.getElementById('addBookBtn');
+addBookBtn.addEventListener('click', bookAdd);
+
 function bookAdd() {
     document.getElementById("book-add-screen").style.width = "100%";
     document.getElementById('page-container').classList.add('blur');
@@ -26,35 +29,101 @@ function exit() {
 
 // Library Array //
 
-let myLibrary = [];
-let newBook;
-
-const addBtn = document.querySelector('#addBtn');
-addBtn.addEventListener('click', addBookToLibrary);
-
-//Book Constructor
-class Book {
-  constructor(title, author, pages, read) {
-      this.title = form.title.value; 
-      this.author = form.author.value; 
-      this.pages = form.pages.value + 'pg'; 
-      this.date = form.date.value;
-      this.read = form.read.checked; 
+// Object Constructor
+function Book(Title, Author, Pages, Started, Read) {
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Started = Started;
+    this.Read = Read;
   }
+
+// Library Array of Objects/Books
+let myLibrary = [];
+
+// Function for adding a new book to array/library
+function addBookToLibrary(Title, Author, Pages, Date, Read) {
+    let book = new Book(Title, Author, Pages, Date, Read);
+    myLibrary.push(book);
 }
 
-function Book() {
-  // the constructor...
+// Function to display library array on cards
+function displayBooksOnPage() {
+    const books = document.querySelector("#hero-card-container");
+
+    // Loop over library array and display to the cards
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement("div");
+        card.classList.add("books");
+        books.appendChild(card);
+        const span = document.createElement("span");
+        card.appendChild(span);
+        const img = document.createElement("img");
+        span.appendChild(img);
+        img.classList.add("icon-or");
+        img.classList.add("remove");
+        img.src = "resources/delete.png";
+
+        for (let key in myLibrary) {
+            const para = document.createElement("p");
+            card.appendChild(para);
+            para.textContent = (`${key}: `)
+            const span = document.createElement("span");
+            para.appendChild(span);
+            span.textContent = (`${myLibrary[key]}`);
+        }
+    })
 }
 
-function addBookToLibrary() {
-  event.preventDefault();
-    popUpForm.style.display = 'none';
+// Get inputs from new book form
 
-    newBook = new Book(title, author, pages, date, read); 
-    myLibrary.push(newBook); 
-    setData();  //saves updated array in local storage
-    render(); 
-    form.reset();
+
+
+// Add Book button push to Book, reset form, close menu
+document.getElementById('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Retrieve HTML Inputs
+    let inputTitle = document.getElementById("book_title");
+    let inputAuthor = document.getElementById("book_author");
+    let inputPages = document.getElementById("book_pages");
+    let inputDate = document.getElementById("book_date");
+    let inputRead = document.getElementById("book_read");
+
+    console.log(inputTitle.value);
+    console.log(inputAuthor.value);
+    console.log(inputPages.value);
+
+    // Assign HTML Inputs to Book, update page
+    addBookToLibrary(inputTitle.value, inputAuthor.value, inputPages.value, inputDate.value, inputRead.value);
+    displayBooksOnPage();
+    updateStats();
+
+    // Reset and quite form
+    document.getElementById('form').reset();
+    document.getElementById("book-add-screen").style.width = "0%";
+    document.getElementById('page-container').classList.remove('blur');
+});
+
+
+// Update in Stats on right side
+function updateStats() {
+    //Total Books
+    let totalBooks = document.querySelector("#total-books");
+    totalBooks.textContent = myLibrary.length;
+
+    //Read = True not working
+    let totalRead = document.querySelector("#read-books");
+    let y = 0;
+    for (let key in myLibrary) {
+        if (myLibrary[key] = true) {
+            y += 1;
+        }
+    }
+    totalRead.textContent = y;
+
+    //Unread
+    let totalUnread = document.querySelector("#unread-books");
+    totalUnread.textContent = myLibrary.length - y;
+
 }
-
