29f0d3fcfbcb9d10d17dab166706a09a3f2023a8
TheDawg101 <curtisgass@hotmail.com>
2022-10-26 00:47:52 -0400

    added new book pop-up and minor fixes to ux

--- script.js
+++ script.js
@@ -11,21 +11,50 @@ function switchTheme(e) {
 }
 toggleSwitch.addEventListener('change', switchTheme, false);
 
+// New Book Menu Overlay //
+
+function bookAdd() {
+    document.getElementById("book-add-screen").style.width = "100%";
+    document.getElementById('page-container').classList.add('blur');
+}
+
+function exit() {
+    document.getElementById("book-add-screen").style.width = "0%";
+    document.getElementById('page-container').classList.remove('blur');
+}
+
 
 // Library Array //
 
 let myLibrary = [];
+let newBook;
+
+const addBtn = document.querySelector('#addBtn');
+addBtn.addEventListener('click', addBookToLibrary);
+
+//Book Constructor
+class Book {
+  constructor(title, author, pages, read) {
+      this.title = form.title.value; 
+      this.author = form.author.value; 
+      this.pages = form.pages.value + 'pg'; 
+      this.date = form.date.value;
+      this.read = form.read.checked; 
+  }
+}
 
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  event.preventDefault();
+    popUpForm.style.display = 'none';
+
+    newBook = new Book(title, author, pages, date, read); 
+    myLibrary.push(newBook); 
+    setData();  //saves updated array in local storage
+    render(); 
+    form.reset();
 }
 
-function bookAdd() {
-    document.getElementById("book-add-screen").style.width = "100%";
-    console.log("test");
-    document.getElementById('page-container').classList.add('blur');
-}
